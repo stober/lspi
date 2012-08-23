ecfc989d2d62e74c8f00a342c787b5f02161fe05
Jeremy Stober <stober@gmail.com>
2012-08-23 11:55:43 -0500

    Single LSPI with option for sparse features. Separate diagnostic class to evaluate performance.

--- src/lspi.py
+++ src/lspi.py
@@ -14,137 +14,73 @@ import numpy.linalg as la
 import sys
 import pickle
 from lstdq import *
-from utils import debugflag, timerflag
+from utils import debugflag, timerflag, consumer
 import scipy.sparse as sp
 
+class Diagnostics:
+    """
+    A class to track the algorithm performance.
+    """
 
-@timerflag
-@debugflag
-def FastLSPI(D, epsilon, env, policy0, save = False, maxiter=10):
+    def __init__(self,env):
+        self.env = env
+        self.Q = np.zeros((env.nstates,env.nactions))
+        self.Qp = np.zeros((env.nstates,env.nactions))
+        self.previous = np.zeros(env.nfeatures)
 
-    current = policy0
-    k = len(policy0)
-    A = sp.dok_matrix((k,k))
-    Q = np.zeros((env.nstates,env.nactions))
-    all_policies = [current]
-    if save:
-        fp = open('weights.pck','w')
-
-    iter = 0
-    while iter < maxiter:
-
-        print "Iteration %d" % iter, " Weight Sum: %f" % np.sum(current)
-
-        previous = current.copy()
-        Ap = A.copy()
-        Qp = Q.copy()
-
-        A,b,current = FastLSTDQ(D, env, previous)
-        all_policies.append(current)
-
-        if save:
-            pickle.dump(current,fp,pickle.HIGHEST_PROTOCOL)
-
-        #pdb.set_trace()
-
-        print "WEIGHT DIFF:", la.norm(previous - current)
-        print "MODEL DIFF:", la.norm((Ap - A).todense())
-
-        # compute s,a matrix of values
-        for i in range(env.nstates):
-            for j in range(env.nactions):
-                Q[i,j] = np.dot(env.phi(i,j),current)
-
-        print "VALUE DIFF:", la.norm(Qp - Q)
-
-        if la.norm(previous - current) < epsilon: break
-
-        iter += 1
-
-        # callback into the environment class for gui stuff?
-        env.callback(iter, current)
-
-    return current, all_policies
-
-
-@timerflag
-@debugflag
-def QR_LSPI(D, epsilon, env, policy0, save=False, maxiter=10):
-
-    current = policy0
-    all_policies = [current]
-    if save:
-        fp = open('weights.pck','w')
-
-    iter = 0
-    while iter < maxiter:
-
-        print "Iteration %d" % iter, " Weight Sum: %f" % np.sum(current)
-
-        previous = current
-        A,b,current = QR_LSTDQ(D, env, previous)
-        all_policies.append(current)
-
-        if save:
-            pickle.dump(current,fp,pickle.HIGHEST_PROTOCOL)
-
-        print "WEIGHT DIFF:", la.norm(previous - current)
-        if la.norm(previous - current) < epsilon: break
-
-        iter += 1
-
-        # callback into the environment class for gui stuff?
-        env.callback(iter, current)
-
-    return current, all_policies
-
-
-
-@timerflag
-@debugflag
-def LSPI(D, epsilon, env, policy0, save=False, maxiter=10):
-
-    current = policy0
-    all_policies = [current]
-    Q = np.zeros((env.nstates,env.nactions))
-    Qp = np.zeros((env.nstates,env.nactions))
-
-    if save:
-        fp = open('weights.pck','w')
-
-    iter = 0
-    while iter < maxiter:
-
-        print "Iteration %d" % iter, " Weight Sum: %f" % np.sum(current)
-
-        previous = current
-        A,b,current = LSTDQ(D, env, previous)
-        all_policies.append(current)
-
-        if save:
-            pickle.dump(current,fp,pickle.HIGHEST_PROTOCOL)
-
-        print "WEIGHT DIFF:", la.norm(previous - current)
-        if la.norm(previous - current) < epsilon: break
-
-        # compute s,a matrix of values
-        for i in range(env.nstates):
-            for j in range(env.nactions):
-                Q[i,j] = np.dot(env.phi(i,j),current)
-
-        print "VALUE DIFF:", la.norm(Qp - Q)
+    def __call__(self,iters, policy):
+            for i in range(self.env.nstates):
+                for j in range(self.env.nactions):
+                    self.Q[i,j] = np.dot(self.env.phi(i,j), policy)
         
-        if iter > 10:
-            pdb.set_trace()
+            result = """
+            Iteration {0}
+            Weight Sum {1}
+            Weight Diff {2}
+            Value Diff {3}
+            """.format(iters,np.sum(policy),la.norm(policy-self.previous),la.norm(self.Q - self.Qp))
+        
+            self.previous[:] = policy[:]
+            self.Qp[:] = self.Q[:]
 
-        Qp[:] = Q[:]
+            # Callback into the environment class for gui stuff.
+            if hasattr(self.env,'callback'):
+                self.env.callback(iters, policy)
+            return result 
+    
 
+@timerflag
+@debugflag
+def LSPI(D, epsilon, env, policy0, method="dense", save=False, maxiter=10, show=False):
 
+    current = policy0
+    all_policies = [current]
 
-        iter += 1
+    if show:
+        diagnostics = Diagnostics(env)
+    if save:
+        fp = open('weights.pck','w')
 
-        # callback into the environment class for gui stuff?
-        env.callback(iter, current)
+    iters = 0
+    while iters < maxiter:
+
+        previous = current
+        if method is "dense":
+            A,b,current = LSTDQ(D, env, previous)
+        elif method is "sparse":
+            A,b,current = QR_LSTDQ(D, env, previous)
+
+        all_policies.append(current)
+
+        if save:
+            pickle.dump(current,fp,pickle.HIGHEST_PROTOCOL)
+
+        if la.norm(previous - current) < epsilon: break
+
+        if show:
+            print diagnostics(iters,current)
+
+        iters += 1
 
     return current, all_policies
 
@@ -158,7 +94,7 @@ def test():
     cw = Chainwalk()
     trace = cw.trace()
     zeros = np.zeros(cw.nfeatures)
-    w = LSPI(trace,0.0001,cw,zeros)
+    w = LSPI(trace,0.0001,cw,zeros,show=True)
     print w
 
 if __name__ == '__main__':
