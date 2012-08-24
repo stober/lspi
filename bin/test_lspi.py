e26e618bed334cad449f43b5ed3be575b0692c04
Jeremy Stober <stober@gmail.com>
2012-08-24 12:51:24 -0500

    Pre-change in sparse matrix type to csr.

--- bin/test_lspi.py
+++ bin/test_lspi.py
@@ -21,10 +21,13 @@ from td import Sarsa
 import cPickle as pickle
 
 # Choose what tests to run.
-run_chainwalk = False
 run_gridworld = False
 rbf_test = False
-test_lspi = True
+
+test_scale= True
+test_chainwalk = False
+test_sarsa = False
+test_lspi = False
 test_walls = False
 
 if test_walls:
@@ -52,62 +55,46 @@ if test_lspi:
         pickle.dump(t,open("lspi_trace.pck","w"),pickle.HIGHEST_PROTOCOL)
     
     policy0 = np.zeros(gw.nfeatures())
-    w0, weights0 = LSPI(t, 0.001, gw, policy0, maxiter=100, method="sparse", debug = False, timer = True, show=True)
+    w0, weights0 = LSPI(t, 0.003, gw, policy0, maxiter=100, method="sparse", debug = False, timer = True, show=True)
     pi = [gw.linear_policy(w0,s) for s in range(gw.nstates)]
     gw.set_arrows(pi)    
     gw.background()
     gw.mainloop()
 
-    # learner = Sarsa(8, 81, 0.5, 0.9, 0.9, 0.1)
-    # learner.learn(10000, gw, verbose=True)
-    # pi = [learner.best(s) for s in range(gw.nstates)]
-    # gw.set_arrows(pi)
-    
+if test_sarsa:
+    gw = GridworldGui(nrows = 9, ncols = 9, endstates = [0], walls = [])
+    learner = Sarsa(8, 81, 0.5, 0.9, 0.9, 0.1)
+    learner.learn(10000, gw, verbose=True)
+    pi = [learner.best(s) for s in range(gw.nstates)]
+    gw.set_arrows(pi)
+    gw.background()
+    gw.mainloop()
+
+if test_chainwalk:
+    cw = Chainwalk()
+    t = cw.trace(1000)
+    policy0 = np.zeros(cw.nfeatures())
+    print LSTDQ(t, cw, policy0)
+
+if test_scale:
+    gw = Gridworld(nrows=32,ncols=32, endstates = [0], walls=[])
+    try:
+        t = pickle.load(open("scale_trace.pck"))
+    except:
+        t = gw.trace(100000, show = False)
+        pickle.dump(t,open("scale_trace.pck","w"),pickle.HIGHEST_PROTOCOL)
+
+    policy0 = np.zeros(gw.nfeatures())
+    w0, weights0 = LSPI(t, 0.005, gw, policy0, maxiter=1, method="sparse", debug = False, timer = True, show=False)
+    # pi = [gw.linear_policy(w0,s) for s in range(gw.nstates)]
+    # gw.set_arrows(pi)    
+    # gw.background()
+    # gw.mainloop()
 
 if rbf_test:
     gw2 = Gridworld2(nrows = 9, ncols = 9, endstates = [0], walls = [], nrbf=15)
     s = gw2.trace(10000)
     policy2 = np.zeros(gw2.nfeatures())
 
-if run_gridworld:
-
-    # stober@stobers-MacBook-Pro:~/Dropbox/workspace/lspi/bin$ python test_lspi.py # 9 x 9
-    # # seconds:  15.244505167
-    # # seconds:  3.86740398407
-    # stober@stobers-MacBook-Pro:~/Dropbox/workspace/lspi/bin$ python test_lspi.py # 18 x 18
-    # # seconds:  461.543802977
-    # # seconds:  4.17448186874
-
-    gw = Gridworld(nrows = 5, ncols = 5, endstates = [0], walls = [])
-    t = gw.trace(10000)
-    policy0 = np.zeros(gw.nfeatures())
-
-    A1,b1,w1 = FastLSTDQ(t, gw, policy0, debug = False, timer=True)
-    A2,b2,w2 = LSTDQ(t, gw, policy0, debug = False, timer=True)
-    A3,b3,w3 = OptLSTDQ(t, gw, policy0, debug = False, timer=True)
-
-
-    print "Fast LSTDQ Error:"
-    print np.sum(np.abs(np.dot(A1.todense(),w1) - b1))
-
-    print "Regular Error:"
-    print np.sum(np.abs(np.dot(A2,w2) - b2))
-
-    print "Opt Error:"
-    print np.sum(np.abs((np.dot(A3,b3) - w3).todense()))
-
-    pdb.set_trace()
-
-    # >>> np.sum(np.abs(np.dot(A1.todense(),w1) - b1))
-    # 0.041507042090663027
-    # >>> np.sum(np.abs(np.dot(A2,w2) - b2))
-    # 0.034436035156235029
-
-
-if run_chainwalk:
-    cw = Chainwalk()
-    t = cw.trace(1000)
-    policy0 = np.zeros(cw.nfeatures)
-    print LSTDQ(t, cw, policy0)
 
 
